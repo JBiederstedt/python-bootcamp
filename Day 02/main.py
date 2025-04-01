4ba5f52aab886f2cd71f99fef9674810fa4e4441
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-01 12:24:54 +0200

    Build Tip Calculator

--- Day 02/main.py
+++ Day 02/main.py
@@ -1,9 +1,7 @@
 print("Welcome to the Tip Calculator!")
 
 total_bill = input("What was the total bill?\n")
-
 tip = input("How much tip would you like to give? 10, 12, or 15 %?\n")
-
 people_to_split = input("How many people will split the bill?\n")
 
 tip_converted = float(tip)/100
