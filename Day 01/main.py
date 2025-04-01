c4e3bc2fc69433c1472db3cc90744627efaba4b0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-01 09:36:32 +0200

    Build Band Name Generator

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,12 +1,8 @@
-#Greeting
 welcome = "Welcome to the Band Name Generator"
 print(welcome)
 
-#Ask the user where they grew up.
 city = input("What's the name of the city you grew up in?\n").capitalize()
 
-#Ask the user for a pet's name.
 pet = input("What was your pet's name? \n").capitalize()
 
-#Combine the name of city and pet's name and show the band name.
 print("Your band name could be " + city + " " + pet)
