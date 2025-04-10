22665a7c4f99e14bc6c8d5817bda88c88c6c083f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-10 14:40:05 +0200

    Build The Quiz Project

--- Day 17/quiz_brain.py
+++ Day 17/quiz_brain.py
@@ -1,8 +1,8 @@
 class QuizBrain:
     def __init__(self, q_list):
+        self.question_list = q_list
         self.question_number = 0
         self.score = 0
-        self.question_list = q_list
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
