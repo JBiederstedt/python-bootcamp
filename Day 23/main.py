4b26242709dd1dd4074c481bdc103be7070a0578
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-14 15:59:02 +0200

    Build Turtle Crossing

--- Day 23/main.py
+++ Day 23/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up", fun=player.move)
@@ -26,10 +27,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.show_game_over()
 
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.increase_car_speed()
+        scoreboard.update_scoreboard()
 
 screen.exitonclick()
--- Day 23/scoreboard.py
+++ Day 23/scoreboard.py
@@ -1,4 +1,21 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.hideturtle()
+        self.penup()
+        self.goto(-290, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {str(self.level)}", align="left", font=FONT)
+
+    def show_game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
