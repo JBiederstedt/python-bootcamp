475945a96ec5e0f2daa4ba4c905effaa7da87dd0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-18 13:46:57 +0200

    Add a count down mechanism

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -17,8 +19,16 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(WORK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(clock, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -28,7 +38,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill=FONT_COLOR_CLOCK, font=(FONT_NAME, FONT_SIZE_CLOCK, FONT_STYLE_CLOCK))
+clock = canvas.create_text(100, 130, text="00:00", fill=FONT_COLOR_CLOCK, font=(FONT_NAME, FONT_SIZE_CLOCK, FONT_STYLE_CLOCK))
 canvas.grid(row=1, column=1)
 
 label_timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, FONT_SIZE_LABEL, FONT_STYLE_LABEL), pady=10)
@@ -37,14 +47,10 @@ label_timer.grid(row=0, column=1)
 label_check = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, FONT_SIZE_LABEL, FONT_STYLE_LABEL))
 label_check.grid(row=3, column=1)
 
-btn_start = Button(text="Start", padx=10, pady=10, borderwidth=0)
+btn_start = Button(text="Start", padx=10, pady=10, borderwidth=0, command=start_timer)
 btn_start.grid(row=2, column=0)
 
 btn_reset = Button(text="Reset", padx=10, pady=10, borderwidth=0)
 btn_reset.grid(row=2, column=2)
-
-
-
-
 
 window.mainloop()
