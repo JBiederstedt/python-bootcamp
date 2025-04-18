1b2adb9b223036ed47e416c193c93def940cc419
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-18 17:05:08 +0200

    Implement saving credentials to file

--- Day 29/main.py
+++ Day 29/main.py
@@ -2,9 +2,29 @@
 
 from tkinter import *
 
+# ---------------------------- CONSTANTS ------------------------------- #
+
+CREDENTIALS_FILE = "credentials.txt"
+DEFAULT_EMAIL   = "placeholder@email.com"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def add_credentials():
+    global CREDENTIALS_FILE, DEFAULT_EMAIL
+
+    website = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+
+    with open(CREDENTIALS_FILE, mode="a", encoding="utf-8") as credentials_file:
+        credentials_file.write(f"Website: {website} | Email/Username: {email} | Password: {password}\n")
+    
+    entry_website.delete(0, END)
+    entry_email.delete(0, END)
+    entry_email.insert(0, DEFAULT_EMAIL)
+    entry_password.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -32,9 +52,11 @@ label_password.grid(row=3, column=0)
 
 entry_website = Entry(width=37)
 entry_website.grid(row=1, column=1, columnspan=2)
+entry_website.focus()
 
 entry_email = Entry(width=37)
 entry_email.grid(row=2, column=1, columnspan=2)
+entry_email.insert(0, DEFAULT_EMAIL)
 
 entry_password = Entry(width=21)
 entry_password.grid(row=3, column=1)
@@ -44,7 +66,7 @@ entry_password.grid(row=3, column=1)
 btn_generate_password = Button(text="Generate Password", width=12)
 btn_generate_password.grid(row=3, column=2)
 
-btn_add_password = Button(text="Add Password", width=35)
-btn_add_password.grid(row=4, column=1, columnspan=2)
+btn_add_credentials = Button(text="Add Credentials", width=35, command=add_credentials)
+btn_add_credentials.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
