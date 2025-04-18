218bc16625b796d08c3e99fe8c8926141f658760
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-18 15:31:27 +0200

    Build Pomodoro Timer

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -14,20 +15,39 @@ FONT_STYLE_LABEL = "normal"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-REPS = 0
+
+# ---------------------------- VARIABLES ------------------------------- #
+
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    global reps, timer
+
+    if timer is not None:
+        window.after_cancel(timer)
+        label_timer.config(text="Timer", fg=GREEN)
+        canvas.itemconfig(clock, text="00:00")
+        label_checks.config(text="")
+        reps = 0
+        timer = None
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+
 def start_timer():
-    global REPS
+    global reps, timer
 
-    REPS += 1
+    if timer is not None:
+        return
 
-    if REPS == 8:
+    reps += 1
+
+    if reps == 8:
         label_timer.config(text="Break", fg=RED)
         count_down(LONG_BREAK_MIN * 60)
-    elif REPS % 2 == 0:
+    elif reps % 2 == 0:
         label_timer.config(text="Break", fg=PINK)
         count_down(SHORT_BREAK_MIN * 60)
     else:
@@ -35,8 +55,10 @@ def start_timer():
         count_down(WORK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
 def count_down(count):
-    global REPS
+    global reps
+    global timer
 
     count_min = count // 60
     count_sec = count % 60
@@ -47,18 +69,19 @@ def count_down(count):
     canvas.itemconfig(clock, text=f"{count_min}:{count_sec}")
 
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
-        if REPS % 2 == 1:  
-            checks = "✔" * ((REPS // 2) + 1)
+        if reps % 2 == 1:  
+            checks = "✔" * ((reps // 2) + 1)
             label_checks.config(text=checks)
 
-        if REPS == 8:
-            REPS = 0
+        if reps == 8:
+            reps = 0
 
         start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -78,7 +101,7 @@ label_checks.grid(row=3, column=1)
 btn_start = Button(text="Start", padx=10, pady=10, borderwidth=0, command=start_timer)
 btn_start.grid(row=2, column=0)
 
-btn_reset = Button(text="Reset", padx=10, pady=10, borderwidth=0)
+btn_reset = Button(text="Reset", padx=10, pady=10, borderwidth=0, command=reset_timer)
 btn_reset.grid(row=2, column=2)
 
 window.mainloop()
