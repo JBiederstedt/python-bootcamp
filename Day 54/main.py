e2ad4c99968d2ec15250b6f9d6159d5e6ceb6a1e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-27 14:43:42 +0200

    Build Python Decorator

---
+++ Day 54/main.py
@@ -0,0 +1,25 @@
+import time
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run speed: {end_time - start_time}s")
+    return wrapper_function
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+current_time = time.time()
+print(current_time)  # shows current time
+
+fast_function()
+slow_function()
